f06c85a94b97f61f0f50e329de58884829db61c0
DavTheDev20 <reiddavin@outlook.com>
2023-09-30 11:25:08 -0400

    Remove unused imports in service

--- src/main/java/com/example/blog/services/PostService.java
+++ src/main/java/com/example/blog/services/PostService.java
@@ -1,19 +1,15 @@
 package com.example.blog.services;
 
-import com.example.blog.MainController;
 import com.example.blog.PostRepository;
 import com.example.blog.models.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class PostService {
 
     @Autowired
     private PostRepository postRepository;
-
 
     public Boolean createPost(String title, String content) {
         try {
@@ -29,7 +25,7 @@ public class PostService {
     }
 
     public Iterable<Post> getPosts() {
-     return postRepository.findAll();
+        return postRepository.findAll();
     }
 
     public Boolean updatePost(int postId, String title, String content) {
